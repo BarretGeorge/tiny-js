29d36dc03401071e9621219e31cb1a1eadcd0f9c
lsm <487005831@qq.com>
2026-01-09 00:31:18 +0800

    jit 减法和除法支持

--- tests/jit_test.cpp
+++ tests/jit_test.cpp
@@ -1,38 +1,46 @@
-#include "object.h"
-#include "parser.h"
-#include "scanner.h"
+#include "jit.h"
 #include <iostream>
+#include <vector>
+#include <cstring>
 
-#include "compiler.h"
-
-constexpr auto SCRIPT = R"(
-    function add(a, b) {
-    return a + b;
+void pushDouble(std::vector<uint8_t>& bc, double val)
+{
+    uint8_t bytes[8];
+    std::memcpy(bytes, &val, 8);
+    for (auto b : bytes) bc.push_back(b);
 }
-
-function multiply(a, b) {
-    return a * b;
-}
-
-let result = add(5, 10);
-
-println("Addition Result: " + result);
-    )";
 
 int main()
 {
-    Scanner scanner(SCRIPT);
-    const auto tokens = scanner.scanTokens();
-    Parser parser(tokens);
+    JitCompiler compiler;
 
-    const auto stmts = parser.parse();
+    std::vector<uint8_t> bytecode;
 
-    VM vm;
-    Compiler compiler(vm);
-    ObjFunction* function = compiler.compile(stmts);
-    for (const auto& byte : function->chunk.code)
+   /**
+    * 计算 (10 + 20) - 5
+    */
+    bytecode.push_back(static_cast<uint8_t>(OpCode::OP_CONSTANT));
+    pushDouble(bytecode, 10.0);
+    bytecode.push_back(static_cast<uint8_t>(OpCode::OP_CONSTANT));
+    pushDouble(bytecode, 20.0);
+    bytecode.push_back(static_cast<uint8_t>(OpCode::OP_ADD));
+    bytecode.push_back(static_cast<uint8_t>(OpCode::OP_CONSTANT));
+    pushDouble(bytecode, 5.0);
+    bytecode.push_back(static_cast<uint8_t>(OpCode::OP_SUB));
+    bytecode.push_back(static_cast<uint8_t>(OpCode::OP_RETURN));
+
+    Chunk chunk;
+    chunk.code = bytecode;
+    if (const auto func = compiler.compile(&chunk))
     {
-        std::cout << opCodeNames[byte] << "\n";
+        double args[1] = {0.0};
+        const double result = func(args);
+        // 输出25
+        std::cout << "JIT 执行结果: " << result << std::endl;
+    }
+    else
+    {
+        std::cout << "JIT 编译失败" << std::endl;
     }
     return 0;
 }
